ff05b4dad6a5e6f29082c6c9cd50eaf218403e1e
abdu1lrahman <abdulrahman.gh2004@gmail.com>
2026-06-28 00:19:17 +0300

    completed firebase connection

--- lib/app.dart
+++ lib/app.dart
@@ -1,26 +1,11 @@
 import 'package:curio/controller/UI/page_indicator_controller.dart';
 import 'package:curio/model/locale/locale.dart';
 import 'package:curio/model/locale/locale_controller.dart';
-import 'package:curio/model/services/storage.dart';
 import 'package:curio/view/screens/onboarding.dart';
 import 'package:curio/view/screens/routes.dart';
 import 'package:curio/view/themes/theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  await StorageService.init();
-  await LocaleService.init();
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(Curio());
-}
 
 class Curio extends StatelessWidget {
   const Curio({super.key});
--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -49,16 +50,16 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDeBtA7VlMyGrDLX1jfsCJmg9O8oLyHWWU',
+  static final FirebaseOptions android = FirebaseOptions(
+    apiKey: dotenv.get('android_api_key'),
     appId: '1:1053543447856:android:47b4c652c715d25d0b1cf5',
     messagingSenderId: '1053543447856',
     projectId: 'curio-a250f',
     storageBucket: 'curio-a250f.firebasestorage.app',
   );
 
-  static const FirebaseOptions ios = FirebaseOptions(
-    apiKey: 'AIzaSyBnDiHonOAV13C2yYipjDFfxLAHAP4iBxY',
+  static final FirebaseOptions ios = FirebaseOptions(
+    apiKey: dotenv.get('ios_api_key'),
     appId: '1:1053543447856:ios:7059cd55a63d82380b1cf5',
     messagingSenderId: '1053543447856',
     projectId: 'curio-a250f',
